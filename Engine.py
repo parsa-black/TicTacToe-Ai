0a7f90a5c980349e49712bc3f95c82a9d04bf282
parsa <parsagorgzan1225@gmail.com>
2023-06-12 00:18:50 +0330

    Create Logical Unit for Game

---
+++ Engine.py
@@ -0,0 +1,11 @@
+from copy import deepcopy
+
+X = "X"
+O = "0"
+EMPTY = None
+
+
+def init_state():
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
