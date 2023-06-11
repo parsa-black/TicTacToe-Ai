fc04e02c2e4df84e539fe48e38491e655884c0ed
parsa <parsagorgzan1225@gmail.com>
2023-06-11 22:57:02 +0330

    init pygame

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Inialize the pygame
+pygame.init()
 
+# Create the screen
+screen = pygame.display.set_mode((800, 600))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
