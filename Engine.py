eec6624a45cd5fca27ff0f1fb14c039ddac74051
parsa <parsagorgzan1225@gmail.com>
2023-06-16 23:14:14 +0330

    add function

--- Engine.py
+++ Engine.py
@@ -9,3 +9,8 @@ def init_state():
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
+
+
+def get_diagonal(board):
+    return [[board[0][0], board[1][1], board[2][2]],
+            [board[0][2], board[1][1], board[2][0]]]
