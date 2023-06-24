c59d6fd49cc882d231a5fe5cdb02d17237eb3d2c
parsa <parsagorgzan1225@gmail.com>
2023-06-24 21:32:01 +0330

    MiniMax AlphaBeta Pruning

--- Engine.py
+++ Engine.py
@@ -24,12 +24,14 @@ def get_columns(board):
     return columns
 
 
-def terminal(row):
+# Win position
+def terminal_win(row):
     return True if row.conunt(row[0]) == 3 else False
 
 
 # return player who has the next turn
 def player(board):
+
     count_X = 0
     count_O = 0
 
@@ -43,3 +45,92 @@ def player(board):
         return O
     else:
         return X
+
+
+# possible actions in the board
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    action = set()
+    for i, row in enumerate(board):
+        for j, worth in enumerate(row):
+            if worth == EMPTY:
+                action.add((i, j))
+    return action
+
+
+# result of actions in the board
+def result(board, action):
+    i, j = action
+    if board[i][j] != EMPTY:
+        raise Exception("Invalid Move")
+    next_move = player(board)
+    deep_board = deepcopy(board)
+    deep_board[i][j] = next_move
+    return deep_board
+
+
+# Return Winner
+def winner(board):
+
+    rows = board + get_diagonal(board) + get_columns(board)
+    for row in rows:
+        current_player = row[0]
+        if current_player is not None and terminal_win(row):
+            return current_player
+    return None
+
+
+# End Position
+def terminal(board):
+    winner_player = winner(board)
+    if winner_player is not None:
+        return True
+    if all(all(j != EMPTY for j in i) for i in board):
+        return True
+    return False
+
+
+def value(board):
+    winner_player = winner(board)
+    if winner_player == X:
+        return 1
+    if winner_player == O:
+        return -1
+    else:
+        return 0
+
+
+# Max alpha-beta Pruning
+def max_pruning(board, alpha, beta):
+    if terminal(board):
+        return value(board), None
+    worth = float("-inf")
+    best = None
+    for action in actions(board):
+        min_worth = max_pruning(result(board, action), alpha, beta)[0]
+        if min_worth > worth:
+            best = action
+            worth = min_worth
+        alpha = max(alpha, worth)
+        if beta <= alpha:
+            break
+    return worth, best
+
+
+# Min alpha-beta Pruning
+def min_alpha_beta_pruning(board, alpha, beta):
+    if terminal(board):
+        return value(board), None
+    worth = float("inf")
+    best = None
+    for action in actions(board):
+        max_val = max_pruning(result(board, action), alpha, beta)[0]
+        if max_val < worth:
+            best = action
+            worth = max_val
+        beta = min(beta, worth)
+        if beta <= alpha:
+            break
+    return worth, best
