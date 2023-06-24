2bc05729f9a6dbc20281fd8543b37268e9df3c63
parsa <parsagorgzan1225@gmail.com>
2023-06-24 19:58:12 +0330

    get_columns

--- Engine.py
+++ Engine.py
@@ -14,3 +14,11 @@ def init_state():
 def get_diagonal(board):
     return [[board[0][0], board[1][1], board[2][2]],
             [board[0][2], board[1][1], board[2][0]]]
+
+
+def get_columns(board):
+    columns = []
+    for i in range(3):
+        columns.append([row[i] for row in board])
+
+    return columns
